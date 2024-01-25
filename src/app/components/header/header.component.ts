0472b0880396e825da4bbea0ddb093f5bcf700bd
Tarek Eldeeb <tarekeldeeb@msn.com>
2024-01-25 17:17:26 +0000

    Added share buttons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewEncapsulation } from '@angular/core';
 import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
@@ -9,9 +9,16 @@ import { NavigationService } from 'src/app/services/navigation.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private navService: NavigationService) { }
+  constructor(
+      private navService: NavigationService,
+      private elementRef:ElementRef
+      ) { }
 
   ngOnInit(): void {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "//cdn.jsdelivr.net/npm/share-buttons/dist/share-buttons.js";
+    this.elementRef.nativeElement.appendChild(s);
   }
 
   toggleSideNav() {
