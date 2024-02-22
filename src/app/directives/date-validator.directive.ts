24a09f26ffa69f3bc24d3b662a9d79cd9fe3b8aa
Tarek Eldeeb <tarekeldeeb@msn.com>
2024-02-22 15:30:40 +0000

    Added Hijri Calendar to set boundary for date

--- src/app/directives/date-validator.directive.ts
+++ src/app/directives/date-validator.directive.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core';
 import { AbstractControl, Validator, NG_VALIDATORS } from '@angular/forms';
+import * as HijriJS from 'hijri-js';
 
 @Directive({
   selector: '[ngxDateValidator]',
@@ -11,7 +12,8 @@ import { AbstractControl, Validator, NG_VALIDATORS } from '@angular/forms';
 })
 export class DateValidatorDirective implements Validator {
     validate(control: AbstractControl) : {[key: string]: any} | null {
-      if (control.value && (control.value<150 || control.value>1445)) {
+      var current_year = HijriJS.initialize().today().year;
+      if (control.value && (control.value<150 || control.value>current_year)) {
         return { 'dateInvalid': true };
       }
       return null;
